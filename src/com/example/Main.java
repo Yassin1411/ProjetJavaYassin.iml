b6c9e129ed55ef753da6bcbf81e39b582433d67f
Yassin1411 <yassin.sti2d@gmail.com>
2023-04-03 00:38:10 +0200

    Derniere Maj du 02/04/2023

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,50 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 
-  //Ajout de import lombok.Getter; et lombok.Setter; (librairie pour éviter d'utiliser les get et set )
-
-  //Faire des tests unitaire 100% (clique droit sur la fonction chercher test)
-
-
-
-  //Ici que des apples de fonctions, replacer les trucs d'en bas dans leurs classes et ne pas oublier de les appeler
-  /*public static void main(String[] args) {
-    // création d'une nouvelle instance de Wand
-    String core="Sureau";
-    int size = 40;
-    Wand wand = new Wand(core, size);
-
-    //Création d'une nouvelle instance de SortingHat
-        SortingHat sortingHat = new SortingHat();
-
-    // Créer une nouvelle instance de Wizard
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Entrez le nom du sorcier : ");
-    String name = scanner.nextLine();
-    int health = 100;
-    int damage = 25;
-        Wizard wizard = new Wizard(name, health, damage, wand);
-
-        // Utiliser les méthodes et propriétés de l'objet Wizard créé
-        System.out.println("Wizard Name : " + wizard.getName());
-        System.out.println("Wizard Health : " + wizard.getHealth());
-        System.out.println("Wizard Damage : " + wizard.getDamage());
-        System.out.println("Size Wand : " + wizard.getWand().getSize());   //METTRE PLUTOT SIZE
-        System.out.println("Core Wand : " + wizard.getWand().getCore());
-
-        // Assigner une maison au sorcier
-        var houses = SortingHat.assignHouse(wizard);
-        System.out.println("La maison assignée à " + wizard.getName() + " est " + houses);
-
-*/
-
-
-
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     //Introduction dans le jeu
-    System.out.println("Bienvenue dans Harry Potter At Home");
+    System.out.println("Bienvenue dans Harry Potter At Home !");
 
     // Create a Wizard
     System.out.println("Enter wizard's name : ");
